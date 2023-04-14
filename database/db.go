0473959af851bafb3782d66316e81369f78b1adc
Arnawa777 <arnawajuan12@gmail.com>
2023-04-15 05:23:54 +0700

    deploy

--- database/db.go
+++ database/db.go
@@ -8,22 +8,34 @@ import (
 	"gorm.io/gorm"
 )
 
+type Database struct {
+	Host     string
+	Username string
+	Password string
+	Port     string
+	Name     string
+}
+
 const (
+	DEBUG_MODE = true
+)
+
+var (
 	DB_HOST     = "localhost"
 	DB_USER     = "postgres"
 	DB_PASSWORD = "admin"
 	DB_NAME     = "my-garm"
 	DB_PORT     = 5432
-	DEBUG_MODE  = true
+	db          *gorm.DB
+	err         error
 )
 
-var (
-	db  *gorm.DB
-	err error
-)
-
-func StartDB() {
+func StartDB(conf *Database) {
 	config := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
+
+	if conf.Host != "" {
+		config = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", conf.Host, conf.Username, conf.Password, conf.Name, conf.Port)
+	}
 
 	db, err = gorm.Open(postgres.Open(config), &gorm.Config{})
 	if err != nil {
--- main.go
+++ main.go
@@ -4,12 +4,29 @@ import (
 	"final-project/database"
 	"final-project/router"
 	"fmt"
+	"os"
 )
 
 func main() {
-	database.StartDB()
 	fmt.Println("Starting database...")
-	router.New().Run(":3000")
+
+	dbConf := database.Database{
+		Host:     os.Getenv("DB_HOST"),
+		Username: os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		Port:     os.Getenv("DB_PORT"),
+		Name:     os.Getenv("DB_NAME"),
+	}
+
+	port := "3000"
+
+	if dbConf.Port != "" {
+		port = os.Getenv("DB_PORT")
+	}
+
+	database.StartDB(&dbConf)
+	// router.New().Run(":3000")
+	router.New().Run(fmt.Sprintf(":%s", port))
 
 	fmt.Println("Starting router on port 3000")
 }
