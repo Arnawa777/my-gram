7785e3063149aa3c0002a3f0b1562408a1b1a86b
Arnawa777 <arnawajuan12@gmail.com>
2023-04-15 06:07:37 +0700

    deploy v3

--- database/db.go
+++ database/db.go
@@ -3,6 +3,7 @@ package database
 import (
 	"final-project/models"
 	"fmt"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -20,22 +21,28 @@ const (
 	DEBUG_MODE = true
 )
 
+// var (
+// 	DB_HOST     = "localhost"
+// 	DB_USER     = "postgres"
+// 	DB_PASSWORD = "admin"
+// 	DB_NAME     = "my-garm"
+// 	DB_PORT     = 5432
+// 	db          *gorm.DB
+// 	err         error
+// )
+
 var (
-	DB_HOST     = "localhost"
-	DB_USER     = "postgres"
-	DB_PASSWORD = "admin"
-	DB_NAME     = "my-garm"
-	DB_PORT     = 5432
+	DB_HOST     = os.Getenv("DB_HOST")
+	DB_USER     = os.Getenv("DB_USER")
+	DB_PASSWORD = os.Getenv("DB_PASSWORD")
+	DB_PORT     = os.Getenv("DB_PORT")
+	DB_NAME     = os.Getenv("DB_NAME")
 	db          *gorm.DB
 	err         error
 )
 
-func StartDB(conf *Database) {
-	config := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
-
-	if conf.Host != "" {
-		config = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", conf.Host, conf.Username, conf.Password, conf.Name, conf.Port)
-	}
+func StartDB() {
+	config := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
 
 	db, err = gorm.Open(postgres.Open(config), &gorm.Config{})
 	if err != nil {
--- main.go
+++ main.go
@@ -10,23 +10,15 @@ import (
 func main() {
 	fmt.Println("Starting database...")
 
-	dbConf := database.Database{
-		Host:     os.Getenv("DB_HOST"),
-		Username: os.Getenv("DB_USER"),
-		Password: os.Getenv("DB_PASSWORD"),
-		Port:     os.Getenv("DB_PORT"),
-		Name:     os.Getenv("DB_NAME"),
-	}
-
 	// port := "3000"
 
 	port := os.Getenv("DB_PORT")
 	// if dbConf.Port != "" {
 	// }
 
-	database.StartDB(&dbConf)
+	database.StartDB()
 	// router.New().Run(":3000")
 	router.New().Run(fmt.Sprintf(":%s", port))
 
-	fmt.Println("Starting router on port 3000")
+	fmt.Println("Starting router on port")
 }
